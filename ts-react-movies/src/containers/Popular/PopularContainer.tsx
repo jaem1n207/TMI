9f6571db864c6048acde136376bd8cfd69d0effe
jaem1n207 <jaem1n207@naver.com>
2020-06-07 08:20:02 +0900

    [ADD] popularContainer

--- ts-react-movies/src/containers/Popular/PopularContainer.tsx
+++ ts-react-movies/src/containers/Popular/PopularContainer.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
-const PopularContainer = () => {
-  return <div></div>;
+import { RootState } from "modules";
+import PopularList from "components/Popular/Popular";
+import { getPopular } from "modules/Popular";
+import LoadingPage from "components/common/LoadingPage/LoadingPage";
+
+interface PopularContainerProps {
+  loading: boolean | undefined;
+  popular: Array<any> | undefined;
+  getPopular: Function;
+}
+const PopularContainer: React.SFC<PopularContainerProps> = ({
+  loading,
+  getPopular,
+  popular,
+}) => {
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  return <>{loading ? <LoadingPage /> : <PopularList popular={popular} />}</>;
 };
 
-export default PopularContainer;
+export default connect(
+  (state: RootState, props) => ({
+    loading: state.popular.loading,
+    popular: state.popular.popular,
+  }),
+  { getPopular }
+)(PopularContainer);
