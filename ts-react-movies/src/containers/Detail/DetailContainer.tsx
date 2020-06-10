ddfcc2154f557548072693c30e99fbf696e448fc
jaem1n207 <jaem1n207@naver.com>
2020-06-10 15:10:47 +0900

    [ADD] DetailContainer

--- ts-react-movies/src/containers/Detail/DetailContainer.tsx
+++ ts-react-movies/src/containers/Detail/DetailContainer.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import DetailList from "components/Detail/Detail";
+import LoadingPage from "components/common/LoadingPage/LoadingPage";
+import { connect } from "react-redux";
+import { RootState } from "modules";
+import { getDetail } from "modules/Detail";
 
-const DetailContainer = () => {
-  return <div></div>;
+interface DetailContainerProps {
+  movie_id: string;
+  loading: boolean | undefined;
+  detail: any;
+  getDetail: Function;
+}
+const DetailContainer: React.SFC<DetailContainerProps> = ({
+  movie_id,
+  loading,
+  detail,
+  getDetail,
+}) => {
+  useEffect(() => {
+    getDetail(movie_id);
+  }, [movie_id]);
+
+  useEffect(() => {
+    return () => {
+      getDetail("");
+    };
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        Object.keys(detail).length > 0 && <DetailList detail={detail} />
+      )}
+    </>
+  );
 };
 
-export default DetailContainer;
+export default connect(
+  (state: RootState) => ({
+    loading: state.detail.loading,
+    detail: state.detail.detail,
+  }),
+  { getDetail }
+)(DetailContainer);
--- ts-react-movies/src/pages/DetailPage.tsx
+++ ts-react-movies/src/pages/DetailPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 import SubPage from "components/SubTemplate/SubTemplate";
 import Navbar from "components/common/Navbar/Navbar";
 import Header from "containers/Header/HeaderContainer";
+import DetailContainer from "containers/Detail/DetailContainer";
 
-interface DetailPageProps {}
+interface MatchParams {
+  movieId: string;
+}
+interface DetailPageProps extends RouteComponentProps<MatchParams> {}
 const DetailPage: React.SFC<DetailPageProps> = (props) => {
+  const { movieId } = props.match.params;
   const Components = {
     Navbar: <Navbar />,
     Header: <Header />,
+    DetailContainer: <DetailContainer movie_id={movieId} />,
   };
   return <SubPage Components={Components}></SubPage>;
 };
