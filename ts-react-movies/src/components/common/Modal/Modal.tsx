fb16551966651daaa5598450ee48124ba24db81a
jaem1n207 <jaem1n207@naver.com>
2020-06-19 13:48:33 +0900

    [ADD] BasicModalComponent

---
+++ ts-react-movies/src/components/common/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import style from "./Modal.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(style);
+
+interface ModalProps {}
+const Modal: React.FC<ModalProps> = () => {
+  return (
+    <>
+      <div className={cx("Modal-Overlay")} />
+      <div className={cx("Modal")}>
+        <div className={cx("Modal-Scroll")}>
+          <div className={cx("Modal-Infor")}>
+            <div className={cx("Modal-Infor-Thum")}>
+              <span>
+                <img src="assets/profile.png" alt="배우 이름" />
+              </span>
+            </div>
+            <div className={cx("Modal-Infor-Article")}>
+              <span className={cx("Modal-Infor-Article-Name")}>송강호</span>
+              <span className={cx("Modal-Infor-Article-Birth")}>
+                1996년 06월 01일 : 2020년 03월 01일
+              </span>
+            </div>
+          </div>
+          <div className={cx("Modal-Infor-Filmography")}>
+            <strong className={cx("Modal-Infor-Filmography-TItle")}>
+              필모그래피
+            </strong>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
---
+++ ts-react-movies/src/pages/DetailCast.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailCast = () => {
+  return <div></div>;
+};
+
+export default DetailCast;
