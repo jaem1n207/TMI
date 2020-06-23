f918a128adb2ab3fa7862af9b2eb8444cf1357fc
jaem1n207 <jaem1n207@naver.com>
2020-06-23 18:04:06 +0900

    [ADD] MorePopular actions

--- ts-react-movies/src/modules/Popular/index.ts
+++ ts-react-movies/src/modules/Popular/index.ts
@@ -5,7 +5,10 @@ import produce from "immer";
 /* Types */
 export interface PopularState {
   loading?: boolean;
+  pages?: number;
+  total_pages?: number;
   popular?: Array<Object>;
+  morePopular?: Array<Object>;
 }
 
 export interface ObjectType {
@@ -24,6 +27,9 @@ export interface ObjectType {
 const GET_POPULAR_REQUEST = "popular/GET_POPULAR_REQUEST";
 const GET_POPULAR_SUCCESS = "popular/GET_POPULAR_SUCCESS";
 const GET_POPULAR_FAIL = "popular/GET_POPULAR_FAIL";
+const GET_MORE_POPULAR_REQUEST = "popular/GET_MORE_POPULAR_REQUEST";
+const GET_MORE_POPULAR_SUCCESS = "popular/GET_MORE_POPULAR_SUCCESS";
+const GET_MORE_POPULAR_FAIL = "popular/GET_MORE_POPULAR_FAIL";
 
 /* Action Creator */
 export const getPopularRequest = (payload: PopularState) => ({
@@ -39,6 +45,20 @@ export const getPopularFail = (payload: PopularState) => ({
   payload,
 });
 
+/* 더보기 */
+export const getMorePopularRequest = (payload: PopularState) => ({
+  type: GET_MORE_POPULAR_REQUEST,
+  payload,
+});
+export const getMorePopularSuccess = (payload: PopularState) => ({
+  type: GET_MORE_POPULAR_SUCCESS,
+  payload,
+});
+export const getMorePopularFail = (payload: PopularState) => ({
+  type: GET_MORE_POPULAR_FAIL,
+  payload,
+});
+
 /* Api actions */
 export const getPopular = (page: number = 1) => {
   return async (dispatch: Dispatch) => {
@@ -47,10 +67,30 @@ export const getPopular = (page: number = 1) => {
 
       const result = await api.movies.popular();
       const popular = result.data.results;
+      const pages = result.data.page;
+      const total_pages = result.data.total_pages;
 
-      dispatch(getPopularSuccess({ loading: false, popular }));
+      dispatch(
+        getPopularSuccess({ loading: false, popular, pages, total_pages })
+      );
     } catch (e) {
       dispatch(getPopularFail({ loading: false }));
+    }
+  };
+};
+
+/* 더보기 */
+export const getMorePopular = (page: number = 2) => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch(getMorePopularRequest({ loading: true }));
+
+      const result = await api.movies.popular();
+      const morePopular = result.data.results;
+
+      dispatch(getMorePopularSuccess({ loading: false, morePopular }));
+    } catch (e) {
+      dispatch(getMorePopularFail({ loading: false }));
     }
   };
 };
@@ -66,6 +106,7 @@ interface Action {
   payload: {
     loading: boolean;
     popular: Array<ObjectType>;
+    morePopular: Array<ObjectType>;
   };
 }
 
@@ -82,6 +123,19 @@ const reducer = (state = initialState, action: Action): PopularState => {
         draft.popular = action.payload.popular;
       });
     case GET_POPULAR_FAIL:
+      return produce(state, (draft) => {
+        draft.loading = action.payload.loading;
+      });
+    case GET_MORE_POPULAR_REQUEST:
+      return produce(state, (draft) => {
+        draft.loading = action.payload.loading;
+      });
+    case GET_MORE_POPULAR_SUCCESS:
+      return produce(state, (draft) => {
+        draft.loading = action.payload.loading;
+        draft.morePopular = action.payload.morePopular;
+      });
+    case GET_MORE_POPULAR_FAIL:
       return produce(state, (draft) => {
         draft.loading = action.payload.loading;
       });
